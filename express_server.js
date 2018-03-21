dff3a6367f69cb541b9224c277b1439bf30ce3b2
Sadie <sadiefreeman@gmail.com>
2018-03-21 00:11:44 +0000

    created POST request, deleted redundant app.js file, added in function to generate random alphanumeric string

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,12 +29,33 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
     longURL: urlDatabase[req.params.id] };
   res.render("urls_shows", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+var text = "";
+var random = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+for (var i = 0; i < 6; i++)
+    text += random.charAt(Math.floor(Math.random() * random.length));
+
+  return text;
+}
+
+console.log(generateRandomString());
